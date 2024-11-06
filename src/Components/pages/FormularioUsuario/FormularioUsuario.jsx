a3336479a75874eecb6b23d0ef000303f334d176
Rosalba <romemo15@gmail.com>
2024-11-06 09:59:21 -0500

    s crea formulario usuario

--- src/Components/pages/FormularioUsuario/FormularioUsuario.jsx
+++ src/Components/pages/FormularioUsuario/FormularioUsuario.jsx
@@ -10,6 +10,20 @@ export function FormularioUsuario(){
     const[fechaUsuario,setFechaUsuario]=useState('')
     const[ciudadUsuario,setCiudadUsuario]=useState('')
 
+    function procesarFormulario(evento){
+        evento.preventDefault()
+        let datosUsuario={
+            nombre:nombreUsuario,
+            edad:edadUsuario,
+            telefono:telefonoUsuario,
+            correo: correoUsuario,
+            contraseña:contraseñaUsuario,
+            fechaRegistro:fechaUsuario,
+            ciudad:ciudadUsuario
+        }
+        console.log(datosUsuario)
+    }
+
     return(
 
         <>
@@ -18,41 +32,77 @@ export function FormularioUsuario(){
                 <div className="row">
                     <div className="col-12">
                         <h3>Registro de usuario: </h3>
-                        <form className="p-5 border rounded shadow">
+                        <form className="p-5 border rounded shadow" onSubmit={procesarFormulario}>
                             <div className="row">
                                 <div className="col-12">
                                     <label className="form-label">Nombre: </label>
-                                    <input type="text" className="form-control" />
+                                    <input
+
+                                        type="text" 
+                                        className="form-control" 
+                                        value={nombreUsuario}
+                                        onChange={function(evento){setNombreUsuario(evento.target.value)}}
+                                    />
                                 </div>
                             </div>
                             <div className="row mt-3">
                                 <div className="col-12 col-md-6">
                                     <label className="form-label">Edad: </label>
-                                    <input type="number" className="form-control" />
+                                    <input 
+                                        type="number" 
+                                        className="form-control" 
+                                        value={edadUsuario}
+                                        onChange={function(evento){setEdadUsuario(evento.target.value)}}
+                                        />
                                 </div>
                                 <div className="col-12 col-md-6">
                                     <label className="form-label">Telefono: </label>
-                                    <input type="number" className="form-control" />
+                                    <input 
+                                        type="number" 
+                                        className="form-control" 
+                                        value={telefonoUsuario}
+                                        onChange={function(evento){setTelefonoUsuario(evento.target.value)}}
+                                    />
                                 </div>
                             </div>
                             <div className="row mt-3">
                                 <div className="col-12 col-md-6">
                                     <label className="form-label">Correo: </label>
-                                    <input type="text" className="form-control" />
+                                    <input 
+                                        type="text" 
+                                        className="form-control"
+                                        value={correoUsuario}
+                                        onChange={function(evento){setCorreoUsuario(evento.target.value)}} 
+                                    />
                                 </div>
                                 <div className="col-12 col-md-6">
                                     <label className="form-label">Contraseña: </label>
-                                    <input type="password" className="form-control" />
+                                    <input 
+                                        type="password" 
+                                        className="form-control"
+                                        value={contraseñaUsuario}
+                                        onChange={function(evento){setContraseñaUsuario(evento.target.value)}}     
+                                    />
                                 </div>
                             </div>
                             <div className="row mt-3">
                                 <div className="col-12 col-md-6">
                                     <label className="form-label">Fecha Registro: </label>
-                                    <input type="date" className="form-control" />
+                                    <input 
+                                        type="date" 
+                                        className="form-control" 
+                                        value={fechaUsuario}
+                                        onChange={function(evento){setFechaUsuario(evento.target.value)}}
+                                        />
                                 </div>
                                 <div className="col-12 col-md-6">
                                     <label className="form-label">Ciudad: </label>
-                                    <input type="text" className="form-control" />
+                                    <input 
+                                        type="text" 
+                                        className="form-control" 
+                                        value={ciudadUsuario}
+                                        onChange={function(evento){setCiudadUsuario(evento.target.value)}}
+                                        />
                                 </div>
                             </div>
                             <button type="submit" className="btn btn-outline-success my-3 w-100">Registrar</button>
