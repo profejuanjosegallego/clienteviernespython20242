a31b1d82d16f3535914d548cd0eb87782739f8bf
Rosalba <romemo15@gmail.com>
2024-11-06 07:58:42 -0500

    s crea cliente home

--- src/Components/common/Acercade/Acercade.jsx
+++ src/Components/common/Acercade/Acercade.jsx
@@ -3,13 +3,13 @@ export function Acercade(){
         <>
             <section className="container my-5">
                 <div className="row">
-                    <div className="col-12 col-md-8">
+                    <div className="col-12 col-md-8 align-self-center">
                         <p>
-                            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed non neque elit. Sed ut tellus eget nisi bibendum tempor. Nulla facilisi. Nulla facilisi. Sed non neque elit. Sed ut tellus eget nisi bibendum tempor. Nulla facilisi. Nulla facilisi. Sed non neque elit. Sed ut tellus eget nisi bibendum tempor. Nulla facilisi. Nulla facilisi. Sed non neque elit. Sed ut tellus eget nisi bibendum tempor. Nulla facilisi. Nulla facilisi. Sed non neque elit. Sed ut tellus eget nisi bibendum tempor. Nulla facilisi. Nulla facilisi. Sed non neque elit. Sed ut tellus eget nisi bibendum tempor. Nulla facilisi. Nulla facilisi. Sed non neque elit. Sed ut tellus eget nisi bibendum tempor. Nulla facilisi. Nulla facilisi. Sed non neque elit. Sed ut tellus eget nisi bibendum tempor. Nulla facilisi. Nulla facil
+                            "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed non neque elit. Sed ut tellus eget nisi bibendum tempor. Nulla facilisi. Nulla facilisi. Sed non neque elit. Sed ut tellus eget nisi bibendum tempor. Nulla facilisi. Nulla facilisi. Sed non neque elit. Sed ut tellus eget nisi bibendum tempor. Nulla facilisi. Nulla facilisi. Sed non neque elit. Sed ut tellus eget nisi bibendum tempor. Nulla facilisi. Nulla facilisi. Sed non neque elit. Sed ut tellus eget nisi bibendum tempor. Nulla facilisi. Nulla facilisi. Sed non neque elit. Sed ut tellus eget nisi bibendum tempor. Nulla facilisi. Nulla facilisi. Sed non neque elit. Sed ut tellus eget nisi bibendum tempor. Nulla facilisi. Nulla facilisi. Sed non neque elit. Sed ut tellus eget nisi bibendum tempor. Nulla facilisi. Nulla facil"
                         </p>
                     </div>
                     <div className="col-12 col-md-4">
-                        <img src="../../../../src/assets/img/personas.webp" alt="foto" 
+                        <img src="../../../../src/assets/img/Grupo_de_personas.png" alt="foto" 
                         className="img-fluid"/>
                     </div>
                 </div>
